2aea0065880596f1eea6f0ea075be405a8b7406c
Harikishen Hosamana <harikishen.hosamana@gmail.com>
2025-06-23 17:12:53 +0530

    chore: add homebrew fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# For Linux
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# For MacOS
+eval "$(/opt/homebrew/bin/brew shellenv)"
